439a0354df489d7cfbdfb9257d607d551136b324
matevarga96 <57194842+matevarga96@users.noreply.github.com>
2020-01-23 03:11:51 +0100

    Update Write.py

--- Write.py
+++ Write.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python      //megadja a terminálnak, hogyan fordítsa a filet, és hogy pythont használjon
 
-import RPi.GPIO as GPIO
-from mfrc522 import SimpleMFRC522
+import RPi.GPIO as GPIO                //Rpi.GPIO-ban vannak azok a funkciók, amik a Pinekkel foglalkoznak, szükségünk lesz ezekre hogy futás után üres legyen minden
+from mfrc522 import SimpleMFRC522      //a SimpleMFRC522 könyvtárt fogjuk használni, hogy kommunikáljunk az RFID RC522-vel, amit könnyebb használni a chip alapkönyvtáránál 
 
-reader = SimpleMFRC522()
+reader = SimpleMFRC522()               //csinál egy objektumot a könyvtárból, és meghívja a setup funkcióját
 
 try:
         text = input('New data:')
