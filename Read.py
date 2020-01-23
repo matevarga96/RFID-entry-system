a37ab863990b9bf41d8b34f61fcba765e4d3d162
Vas Richárd Csaba <58149185+RichardVas@users.noreply.github.com>
2020-01-23 03:37:53 +0100

    Update Read.py

--- Read.py
+++ Read.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python                           #megadja a terminálnak, hogyan fordítsa a filet, és hogy pythont használjon
 
-import RPi.GPIO as GPIO
-from mfrc522 import SimpleMFRC522
+                                
+import RPi.GPIO as GPIO                         #Rpi.GPIO-ban vannak azok a funkciók, amik a Pinekkel foglalkoznak
 
-reader = SimpleMFRC522()
+reader = SimpleMFRC522()                        #csinál egy objektumot a könyvtárból, és meghívja a setup funkcióját
 
 try:
-        id, text = reader.read()
-        print(id)
-        print(text)
-finally:
-        GPIO.cleanup()
+        id, text = reader.read()                #a read funkció meghívása, beolvasás
+        print(id)                               
+        print(text)                             #beolvasott adatok megjelenítése
+finally:                                        #try után fut le
+        GPIO.cleanup()                          #feltakarítunk magunk után
