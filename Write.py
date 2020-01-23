f53c84a2c5cf2f906874e26c49d159c7c7455604
matevarga96 <57194842+matevarga96@users.noreply.github.com>
2020-01-23 03:39:31 +0100

    Update Write.py

--- Write.py
+++ Write.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python      #megadja a terminálnak, hogyan fordítsa a filet, és hogy pythont használjon
+#!/usr/bin/env python                   #megadja a terminálnak, hogyan fordítsa a filet, és hogy pythont használjon
 
-import RPi.GPIO as GPIO                #Rpi.GPIO-ban vannak azok a funkciók, amik a Pinekkel foglalkoznak, szükségünk lesz ezekre hogy futás után üres legyen minden
-from mfrc522 import SimpleMFRC522      #a SimpleMFRC522 könyvtárt fogjuk használni, hogy kommunikáljunk az RFID RC522-vel, amit könnyebb használni a chip alapkönyvtáránál 
+import RPi.GPIO as GPIO                 #Rpi.GPIO-ban vannak azok a funkciók, amik a Pinekkel foglalkoznak, szükségünk lesz ezekre hogy futás után üres legyen minden
+from mfrc522 import SimpleMFRC522       #a SimpleMFRC522 könyvtárt fogjuk használni, hogy kommunikáljunk az RFID RC522-vel, amit könnyebb használni a chip alapkönyvtáránál 
 
-reader = SimpleMFRC522()               #csinál egy objektumot a könyvtárból, és meghívja a setup funkcióját
+reader = SimpleMFRC522()                #csinál egy objektumot a könyvtárból, és meghívja a setup funkcióját
 
-try:                                                  #try-jal írjuk, hogy el tudjuk csípni az esetleges rendellenes viselkedését a kódnak
-        text = input('New data:')                     #beolvas a parancssorról, eltárolja egy text változóban
-        print("Now place your tag to write")          #mehet a kártya az olvasóhoz
-        reader.write(text)                            #a tagre írjuk a változónkat
-        print("Written")                              #sikeres rögzítés
-finally:                                              #try után fut le
-        GPIO.cleanup()                                #feltakarítunk magunk után
+try:                                                    #try-jal írjuk, hogy el tudjuk csípni az esetleges rendellenes viselkedését a kódnak
+        text = input('New data:')                       #beolvas a parancssorról, eltárolja egy text változóban
+        print("Now place your tag to write")            #mehet a kártya az olvasóhoz
+        reader.write(text)                              #a tagre írjuk a változónkat
+        print("Written")                                #sikeres rögzítés
+finally:                                                #try után fut le
+        GPIO.cleanup()                                  #feltakarítunk magunk után
